9702e208a1d73b3bce9e49213dd6c031bc62e91e
SBM <manjusree1062@gmail.com>
2019-10-27 12:25:16 +0530

    Updates

---
+++ precache-manifest.ecaffcf5a1ffff6405476dcb8666cc92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44e460e2bf3ba5567661",
+    "url": "/arihant/static/css/main.256ab9dc.chunk.css"
+  },
+  {
+    "revision": "44e460e2bf3ba5567661",
+    "url": "/arihant/static/js/main.44e460e2.chunk.js"
+  },
+  {
+    "revision": "8153b5790d4c42df84da",
+    "url": "/arihant/static/js/1.8153b579.chunk.js"
+  },
+  {
+    "revision": "6efa410f9761e1eae6e1",
+    "url": "/arihant/static/js/runtime~main.6efa410f.js"
+  },
+  {
+    "revision": "04614878557a05993be308e5774b4b9c",
+    "url": "/arihant/index.html"
+  }
+];
